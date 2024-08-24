bacded230147ab38fa21efe783b5d2a29dee660b
Suryadev78 <pandeysuryadev00@gmail.com>
2024-08-24 23:56:14 +0530

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Description from "./pages/Description";
 
 function App() {
+  const { search, pathname } = useLocation();
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/description/id?" element={<Description />}></Route>
-        </Routes>
-      </BrowserRouter>
+      {pathname != "/" && search.length > 0}
+      <div>
+        <div className="flex justify-center font-semibold cursor-pointer bg-gray-300">
+          Go to Home
+        </div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/description/id?" element={<Description />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import productData from "../store/atom";
 import axios from "axios";
 
@@ -9,47 +9,53 @@ export default function Home() {
   const [product, setProduct] = useRecoilState(productData);
   const [searchParams] = useSearchParams();
   const item = searchParams.get("category");
-  const [filteredProducts, setFilteredProducts] = useState(null);
-  // console.log(item);
-  // const item = useRecoilValue(productData);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  // Fetch all products initially
   useEffect(() => {
     axios
       .get("https://fakestoreapi.com/products")
-      .then((res) => setProduct(res.data));
-  }, []);
+      .then((res) => setProduct(res.data))
+      .catch((err) => console.error("Error fetching products: ", err));
+  }, [setProduct]);
+
+  // Update filtered products based on the selected category or show all products
   useEffect(() => {
-    if (!filteredProducts) {
-      setFilteredProducts(product);
+    if (!item) {
+      setFilteredProducts(product); // Show all products if no category is selected
+    } else {
+      axios
+        .get(`https://fakestoreapi.com/products/category/${item}`)
+        .then((res) => setFilteredProducts(res.data))
+        .catch((err) =>
+          console.error("Error fetching filtered products: ", err)
+        );
     }
-    axios
-      .get(`https://fakestoreapi.com/products/category/${item}`)
-      .then((res) => setFilteredProducts(res.data));
-  }, [item]);
-  console.log(filteredProducts);
-  // console.log(filteredProducts);
+  }, [item, product]);
 
   const navigate = useNavigate();
+
   return (
     <div className="flex w-full h-screen bg-zinc-100">
-      <Navbar></Navbar>
+      <Navbar />
       <div className="flex p-16 h-full  w-[85%] gap-1 flex-wrap  overflow-x-hidden overflow-y-auto">
-        {filteredProducts &&
-          filteredProducts.map((i) => {
-            return (
-              <div
-                onClick={() => navigate(`/description?id=${i.id}`)}
-                className="mr-3 cursor-pointer mt-1 br-2 p-1 border shadow-sm rounded w-[20%] h-[35vh] flex flex-col justify-center items-center"
-              >
-                <div className="w-28 h-32 object-contain">
-                  <img
-                    className="hover:scale-95 w-full h-[80%]"
-                    src={i.image}
-                  />
-                </div>
-                <p className="text-center text-xs">{i.title}</p>
+        {filteredProducts.length > 0 &&
+          filteredProducts.map((i) => (
+            <div
+              key={i.id}
+              onClick={() => navigate(`/description?id=${i.id}`)}
+              className="mr-3 cursor-pointer mt-1 br-2 p-1 border shadow-sm rounded w-[20%] h-[35vh] flex flex-col justify-center items-center"
+            >
+              <div className="w-28 h-32 object-contain">
+                <img
+                  className="hover:scale-95 w-full h-[80%]"
+                  src={i.image}
+                  alt={i.title}
+                />
               </div>
-            );
-          })}
+              <p className="text-center text-xs">{i.title}</p>
+            </div>
+          ))}
       </div>
     </div>
   );
