996c7929389558583396198749a7d196f4a0c84d
Suryadev78 <pandeysuryadev00@gmail.com>
2024-09-11 23:41:33 +0530

    made some changes to Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -54,6 +54,7 @@ export default function Home() {
                 />
               </div>
               <p className="text-center text-xs">{i.title}</p>
+              <div>hello</div>
             </div>
           ))}
       </div>
